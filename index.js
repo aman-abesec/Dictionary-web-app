9d0a5c2e4d41325600c4a5196e0141141baf3724
Aman Singh <92168231+aman-abesec@users.noreply.github.com>
2023-02-11 15:14:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var audio_array=['https://api.dictionaryapi.dev/media/pronunciations/en/keyboard-us.mp3']
 const link='https://api.dictionaryapi.dev/api/v2/entries/en/';
 var searchBox=document.querySelector('#searchBox');
 var searchBtn=document.querySelector('#searchBtn');
@@ -30,17 +31,18 @@ searchBtn.addEventListener('click',function(){
     example.innerHTML="\""+data[0].meanings[1].definitions[0].example+"\"";
     sourceUrls.setAttribute('href',data[0].sourceUrls[0]);
     sourceUrls.innerHTML=data[0].sourceUrls[0];
-    var audio=data[0].phonetics[2].audio;
+    audio_array[0]=data[0].phonetics[2].audio;
+    // alert(audio);
   })
 });
 
 var audioValue=document.querySelector('.audio');
 audioValue.addEventListener('click',function(){
     var x = document.createElement("AUDIO");
-    if (x.canPlayType("audio/mpeg")) {
-      x.setAttribute("src",audio);
+    if (x.canPlayType("audio/mp3")) {
+      x.setAttribute("src",audio_array[0]);
     } else {
-      x.setAttribute("src",audio);
+      x.setAttribute("src",audio_array[0]);
     }
     x.setAttribute("controls", "controls");
     x.play();
